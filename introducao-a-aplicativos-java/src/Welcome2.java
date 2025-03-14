dcf7f36767730619071c2d4a06b8ce96375c9181
Jair Marques <jairsmarques1126@gmail.com>
2025-03-13 21:20:11 -0300

    modificando o primeiro programa Java

---
+++ introducao-a-aplicativos-java/src/Welcome2.java
@@ -0,0 +1,14 @@
+// Figura 2.3: Welcome2.java
+// Imprimindo uma linha de texto com múltiplas instruções.
+
+public class Welcome2 {
+
+	// método main inicia a execução do aplicativo Java
+	public static void main(String[] args) {
+
+		System.out.print("Welcome to ");
+		System.out.println("Java Programming!");
+
+	} // fim de método main
+
+} // fim da clase Welcome2
